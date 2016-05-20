5107eb4552945693cfa6842a3a3a786a8747529b
nagane <naganeys@gmail.com>
2016-05-20 14:07:04 +0900

    [wip] zabbix recipe

--- zabbix/recipes/server.rb
+++ zabbix/recipes/server.rb
@@ -1,19 +1,23 @@
 
-include_recipe "yum-repo::default"
+#include_recipe "yum-repo::default"
 
 rpm_package 'install zabbix release' do
   source 'http://repo.zabbix.com/zabbix/3.0/rhel/6/x86_64/zabbix-release-3.0-1.el6.noarch.rpm'
 end
 
 
-%w{fping curl curl-devel OpenIPMI OpenIPMI-devel net-snmp net-snmp-devel unixODBC unixODBC-devel iksmel iksemel-devel }.each do |pkg|
+%w{fping curl curl-devel OpenIPMI OpenIPMI-devel net-snmp net-snmp-devel unixODBC unixODBC-devel zabbix-server-mysql zabbix-web-mysql zabbix-web-japanese }.each do |pkg|
   package pkg do
     action :install
   end
-  
 end
 
-#rpm_package 'install zabbix server' do
-#  source 'http://repo.zabbix.com/zabbix/3.0/rhel/6/x86_64/zabbix-server-2.4.8-1.el6.x86_64.rpm'
-#end
+execute "create mysql db" do
+  command "mysql -e \"create database zabbix;\""
+  not_if "mysql -e \"show databases;\" |grep zabbix"
+end
 
+execute "create mysql recorde" do
+  command "zcat /usr/share/doc/zabbix-server-mysql-3.0.2/create.sql.gz | mysql -uroot zabbix"
+  only_if { File.directory?("/usr/share/doc/zabbix-server-mysql-3.0.2")}
+end
