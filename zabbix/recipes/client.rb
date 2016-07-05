c50cb006f761782edfaf3197d51d0317582e15f2
nagane <naganeys@gmail.com>
2016-07-05 19:05:46 +0900

    add config file

---
+++ zabbix/recipes/client.rb
@@ -0,0 +1,16 @@
+
+rpm_package 'install zabbix release' do
+  source 'http://repo.zabbix.com/zabbix/3.0/rhel/6/x86_64/zabbix-release-3.0-1.el6.noarch.rpm'
+end
+
+%w{ zabbix-agent zabbix-sender }.each do |pkg|
+  package pkg do
+    action :install
+  end
+end
+
+
+template '/etc/zabbix/zabbix_agentd.conf' do
+  source 'zabbix_agentd.conf.erb'
+  #notifies :restart
+end
